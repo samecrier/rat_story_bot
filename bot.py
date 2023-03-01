0b94fb0fa87612829168a126b60617f5be8d5973
samecrier <chentcovru@gmail.com>
2023-03-01 22:51:21 +0600

    test new infinity_polling + add answer about amount of updates

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import config
 import telebot
 import instagram_parser as ip
+from time import sleep
+
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
@@ -9,11 +11,15 @@ def answer_to_message(message):
 	positive_results = ['обнови', 'алешина', 'алёшина', 'инст', 'давай']
 	if message.text.lower() in positive_results:
 		bot.reply_to(message, 'Я проверяю обновления')
-		ip.check_stories()
-		bot.reply_to(message, 'Я добавил все новые обновления')
+		x = ip.check_stories()
+		if x == 0:
+			bot.reply_to(message, 'Нет новых обновлений')
+		elif x == 1:
+			bot.reply_to(message, f"Добавлено {x} новое обновление")
+		else:
+			bot.reply_to(message, f"Добавлено {x} новых обновлений")
 	else:
 		bot.reply_to(message, 'Использую одно из слов чтобы обновить ленту: обнови, алешина, алёшина, инст, давай')
 
 
-
-bot.infinity_polling()
+bot.infinity_polling(timeout=60, long_polling_timeout = 10)
